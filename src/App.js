7097e7c209e8c4cabeba78012808866d5d1d10f5
Pechenyice <german.iskhakov@mail.ru>
2021-07-19 13:21:10 +0300

    dynamic app.js changes

--- src/App.js
+++ src/App.js
@@ -6,33 +6,35 @@ import './App.css';
 import NewRound from "./components/NewRound/NewRound";
 import Controls from "./components/Controls/Controls";
 import Help from "./components/Help/Help";
+import Hints from "./components/Hints/Hints";
 
 function App() {
 
   const [state, setState] = useState(
       {
-        tournamentStarted: !!localStorage.getItem('tournamentStarted')
+        tournamentStarted: localStorage.getItem('tournamentStarted')
       }
   );
 
   function handleCLickStartTournament(e) {
-    localStorage.setItem('tournamentStarted', true);
-    setState({tournamentStarted: !!localStorage.getItem('tournamentStarted')});
+    localStorage.setItem('tournamentStarted', 1);
+    setState({tournamentStarted: 1});
   }
 
   function handleCLickEndTournament(e) {
-    localStorage.setItem('tournamentStarted', true);
-    setState({tournamentStarted: !!localStorage.getItem('tournamentStarted')});
+    localStorage.setItem('tournamentStarted', 0);
+    setState({tournamentStarted: 0});
   }
 
   return (
     <div className="App">
-      <TournamentTable />
+      <TournamentTable tournamentStarted={state.tournamentStarted} team={state.tournamentStarted ? state.currentTeam : state.lastTeam} />
       <Logo />
       <NewTournament shouldRender={!state.tournamentStarted} onStart={handleCLickStartTournament} />
       <NewRound shouldRender={state.tournamentStarted} />
       <Controls onEnd={handleCLickEndTournament} />
       <Help />
+      <Hints />
     </div>
   );
 }
